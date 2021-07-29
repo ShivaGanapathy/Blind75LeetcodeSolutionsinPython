de0c8e97b949408b7e7cce7a5a7c02febd6a2574
ShivaGanapathy <47366124+ShivaGanapathy@users.noreply.github.com>
2021-07-29 17:31:36 -0400

    Update ConstructBinaryTreefromPreorderandInorderTraversal.py

--- ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -7,12 +7,14 @@
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         
-        if not preorder[0]:
+        if not preorder or not inorder:
             return None
-        else:
-            root_val = preorder[0]
-            root = TreeNode(root_val)
-            mid  = inorder.index(root_val)
-            root.left = seld.buildTree(preorder[1:],inorder[1:mid+1])
-            root.right = seld.buildTree(preorder[1:],inorder[mid+1:])
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
+        
+        return root
+    
             
