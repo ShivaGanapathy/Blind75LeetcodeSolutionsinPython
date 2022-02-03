e02ec5b364c1ed8f5bf1ececf20b05cf2f3502d1
ShivaGanapathy <47366124+ShivaGanapathy@users.noreply.github.com>
2022-02-03 07:54:35 -0500

    Update LRUCache.py

--- LRUCache.py
+++ LRUCache.py
@@ -1,73 +1,82 @@
-class Node:
+class ListNode():
+    
     def __init__(self,key,value):
-        self.key = key
-        self.value = value
-        self.next = None
         self.prev = None
+        self.next = None
+        self.key = key
+        self.val = value
         
-    def setNext(self,nextNode):
-        self.next = nextNode
-    def setPrev(self,prevNode):
-        self.prev = prevNode
-    def setValue(self,value):
-        self.value = value
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.cache = {}
-        self.size = 0
-        self.head = Node(-1,-1)
-        self.tail = Node(-1,-1)
-        self.head.setNext(self.tail)
-        self.head.setPrev(self.head)
         
-    def deleteNode(self,curNode):
-        prevNode = curNode.prev
-        nextNode = curNode.next
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-        self.size-=1
-    
-    def insertNode(self,curNode):
-        nextNode = self.head.next
-        self.head.next = curNode
-        curNode.prev = self.head
-        curNode.next = nextNode
-        nextNode.prev = curNode
-        self.size+=1
+        
+    def detach_node(self, node):
+        after = node.next
+        before = node.prev
+        before.next = after
+        after.prev = before
         
     
+    
+    def attach_to_head(self, node):
+        first = self.head.next
+        self.head.next = node
+        first.prev = node
+        node.prev = self.head
+        node.next = first
+    
+    def evict_node(self):
+        remove = self.tail.prev
+        before = remove.prev
+        before.next = self.tail
+        self.tail.prev = before
+        del self.cache[remove.key]
+    
+    
+        
+        
+        
 
-    def get(self, key: int):
+    def get(self, key: int) -> int:
+        # Either it's in the cache or its not
+        
+        #If it is
         if key in self.cache:
-            curNode = self.cache[key][1]
-            self.deleteNode(curNode)
-            self.insertNode(curNode)
-            return self.cache[key][0]
+            node = self.cache[key]
+            self.detach_node(node)
+            self.attach_to_head(node)
+            return node.val
+        #if its not
         else:
             return -1
-        
 
     def put(self, key: int, value: int) -> None:
+        #Either it's already in the cache or its not
+        
         if key in self.cache:
-            #push the key node to the head
-            curNode = self.cache[key][1]
-            self.deleteNode(curNode)
-            self.insertNode(curNode)
-            #update value at index key
-            self.cache[key][0] = value
-            
+            node = self.cache[key]
+            node.val = value
+            self.detach_node(node)
+            self.attach_to_head(node)
         else:
-            curNode = Node(key,value)
-            self.insertNode(curNode)
-            self.cache[key] = [value,curNode]
+            node = ListNode(key,value)
+            self.cache[key] = node
+            self.attach_to_head(node)
+            if len(self.cache) > self.capacity:
+                self.evict_node()
             
-            
-        if self.size > self.capacity:
-            #get rid of lru
-            lruNode = self.tail.prev
-            self.deleteNode(lruNode)
-            del self.cache[lruNode.key]
         
 
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
